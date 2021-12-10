c678dfa4fbbb75aa22f3563acf2404024207f3eb
MNoble2014 <michealsimpson2013@gmail.com>
2021-12-09 20:06:05 -0500

    data.js updated

--- src/Components/data.js
+++ src/Components/data.js
@@ -6,7 +6,9 @@ var moment = require("moment");
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const begin = moment().utcOffset(timezone)._d;
 const max = moment().utcOffset(timezone)._d;
-const currentTime = moment().utcOffset(timezone.format());
+const currentTime = moment()
+    .utcOffset(timezone)
+    .format();
 
 export default class Data extends Component {
     constructor(props) {
@@ -19,6 +21,7 @@ export default class Data extends Component {
         };
         this.handleChange = this.handleChange.bind(this);
     }
+
     handleChange(date) {
         this.setState(
             {
@@ -28,7 +31,48 @@ export default class Data extends Component {
             () => {}
         );
         setTimeout(
-            axios.get('')
+            axios
+                .get('https://api.nasa.gov/planetary/apod?api_key=X7831OHO7jNbCUFp6ZquUbFjl2lxHRDvsbay1fU4&date${
+                    this.state.startDate === "2021-12009"
+                    ?this.state.startDate.toISOString().slice(0, -14)
+                    :date.toISOString().slice(0, -14)
+                }   
         )
-    }
+        .then(response => {
+            this.setState({
+                data: [response.data]
+            });
+        }),
+    }),
+    3000
+    );
 }
+
+// Initial Data
+componentDidMount() {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=X7831OHO7jNbCUFp6ZquUbFj12txHRDvsbay1fU4&date=${this.state.startDate.slice'(
+        0,
+        -15
+    ))
+}
+.then(response => {
+    this.setState({
+        data: [response.data]
+    });
+})
+
+render() {
+    return (
+        <>
+        <h1>Select Date:</h1>
+        {/*Displays Calendar */}
+        <DatePicker
+        maxDate={this.state.max}
+        selected={this.state.begin}
+        onChange={this.handleChange}
+        />
+        <Cards data={this.state.data}/>
+        </>
+    );
+}
+
